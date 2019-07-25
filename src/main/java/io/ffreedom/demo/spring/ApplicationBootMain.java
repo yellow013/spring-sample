ac2da220d7899d095b1b0f7a863cec5dbef6a5e8
yellow013 <wk9988@gmail.com>
2019-07-25 13:58:56 +0800

    init commits

---
+++ src/main/java/io/ffreedom/demo/spring/ApplicationBootMain.java
@@ -0,0 +1,13 @@
+package io.ffreedom.demo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootMain.class, args);
+	}
+
+}
